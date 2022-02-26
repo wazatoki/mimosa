0dbe876e2a547262eb105b5ec0a27eb0cd17955a
wazatoki <wazatoki@gmail.com>
2022-02-26 11:24:47 +0000

    add stockRecieve update

--- app/repositories/stockReceive.js
+++ app/repositories/stockReceive.js
@@ -20,3 +20,23 @@ export async function insert(stockRecieve, ope_staff_id) {
 
     return params.id
 }
+
+export async function update(stockRecieve, ope_staff_id) {
+
+    const params = {
+        id: stockRecieve.id,
+        created_at: new Date(),
+        created_by: ope_staff_id,
+        operated_at: new Date(),
+        operated_by: ope_staff_id,
+        name: stockRecieve.name,
+        slip_id: stockRecieve.slipID,
+        slip_date: stockRecieve.slipDate,
+        picture_path: stockRecieve.picturePath
+    };
+
+    await none('update stock_receive '
+        + 'set operated_at=$(operated_at), operated_by=$(operated_by), name=$(name), slip_id=$(slip_id), '
+        + 'slip_date=${slip_date}, picture_path=${picture_path} '
+        + 'where id=$(id)', params);
+}
--- app/repositories/stockRecieve.test.js
+++ app/repositories/stockRecieve.test.js
@@ -24,3 +24,33 @@ test("stockReceive insert", async () => {
     expect(stockReceive.picturePath).toEqual(result.picture_path);
 
 });
+
+test("stockReceive update", async () => {
+
+    const id = createUUID();
+    const params = {
+        id: id,
+        created_at: new Date(),
+        created_by: "test_staff_id_1",
+        operated_at: new Date(),
+        operated_by: "test_staff_id_1",
+        name: "test_name_1",
+        slip_id: 'test0001',
+        slip_date: new Date(2020,1,1),
+        picture_path: 'test_path'
+    };
+
+    await none('insert into stock_receive(${this:name}) values(${this:csv})', params);
+
+    const stockReceive = new StockRecieve("test_name_2", "test0002", new Date(2020,1,2), "test_path_2")
+    stockReceive.id = id
+
+
+    await update(stockReceive, "test_staff_id_2");
+    const result = await one('select name, slip_id, slip_date, picture_path from stock_receive where id=$1', id);
+
+    expect(stockReceive.name).toBe(result.name);
+    expect(stockReceive.slipID).toEqual(result.slip_id);
+    expect(stockReceive.slipDate).toEqual(result.slip_date);
+    expect(stockReceive.picturePath).toEqual(result.picture_path);
+});
