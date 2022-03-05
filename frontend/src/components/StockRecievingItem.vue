94432a8130afcc4549f6a1f574e70a73074fb7dc
wazatoki <wazatoki@gmail.com>
2022-03-05 13:47:51 +0000

    add remove recieving item

--- frontend/src/components/StockRecievingItem.vue
+++ frontend/src/components/StockRecievingItem.vue
@@ -17,10 +17,18 @@
           </el-option>
         </el-select>
       </el-col>
-      <el-col :span="10"
-        ><el-input @blur="emitData" v-model="quantity" class="form-input" placeholder="数量" />
+      <el-col :span="8"
+        ><el-input
+          @blur="emitData"
+          v-model="quantity"
+          class="form-input"
+          placeholder="数量"
+        />
       </el-col>
       <el-col :span="2">{{ unitName }}</el-col>
+      <el-col :span="2">
+        <el-button type="warning" @click="clickDelete">行削除</el-button>
+      </el-col>
     </el-row>
   </div>
 </template>
@@ -29,8 +37,9 @@
 import { ref } from "vue";
 
 export default {
-    props: ['recievingData', 'itemMsts'],
-    emits: ['update:recievingData'],
+  name: "StockRecievingItem",
+  props: ["recievingData", "itemMsts"],
+  emits: ["update:recievingData", "deleteItem"],
   setup(props, { emit }) {
     const quantity = ref(props.recievingData.quantity);
     const unitName = ref(props.recievingData.item.receivingUnit.name);
@@ -42,13 +51,24 @@ export default {
       emitData();
     };
     const emitData = () => {
-      emit('update:recievingData', {
+      emit("update:recievingData", {
+        id: props.recievingData.id,
         item: props.itemMsts.find((item) => item.id === selectedItemID.value),
-        quantity: quantity.value
+        quantity: quantity.value,
       });
     };
+    const clickDelete = () => {
+      emit("deleteItem");
+    };
 
-    return { selectedItemID, unitName, quantity, onChange, emitData };
+    return {
+      selectedItemID,
+      unitName,
+      quantity,
+      onChange,
+      emitData,
+      clickDelete,
+    };
   },
 };
 </script>
--- frontend/src/views/StockRecieving.vue
+++ frontend/src/views/StockRecieving.vue
@@ -2,13 +2,13 @@
   <div class="Stock">
     <el-container>
       <el-header>
-        <Header title="入荷"></Header>
+        <Header title="入荷" />
       </el-header>
       <el-main>
-        <el-form ref="formRef" :model="form" label-width="120px">
+        <el-form ref="formRef" label-width="120px">
           <el-form-item label="入荷日">
             <el-date-picker
-              v-model="form.actDate"
+              v-model="actDate"
               type="date"
               placeholder="Pick a day"
             >
@@ -16,24 +16,27 @@
           </el-form-item>
           <el-form-item label="仕入先">
             <el-input
-              v-model="form.recieveFrom"
+              v-model="recieveFrom"
               class="form-input"
               placeholder="Please input"
             />
           </el-form-item>
 
           <el-form-item
-            v-for="(recievingData, index) in form.recievingDataArray"
-            :key="index"
+            v-for="(recievingData, index) in recievingDataArray"
+            :key="recievingData.id"
           >
             <stock-recieving-item
-              v-model:recievingData="form.recievingDataArray[index]"
-              :itemMsts="itemMsts"
+              v-model:recieving-data="recievingDataArray[index]"
+              :item-msts="itemMsts"
+              @deleteItem="removerecievingData(index)"
             ></stock-recieving-item>
           </el-form-item>
+          <el-button type="primary" @click="addRecievingData">行追加</el-button>
           <el-button type="primary" @click="onSubmit">Create</el-button>
           <el-button>Cancel</el-button>
         </el-form>
+        {{ recievingDataArray }}
       </el-main>
     </el-container>
   </div>
@@ -44,7 +47,8 @@
 import Header from "@/components/Header.vue";
 import StockRecievingItem from "@/components/StockRecievingItem.vue";
 import { Item } from "@/models/recieve";
-import { reactive } from "vue";
+import { reactive, ref } from "vue";
+import { createUUID } from "@/utils/string"
 
 export default {
   name: "StockRecieving",
@@ -52,69 +56,24 @@ export default {
     Header,
     StockRecievingItem,
   },
-  data() {
-    return {
-      form: reactive({
-        actDate: new Date(),
-        recieveFrom: "",
-        recievingDataArray: [
-          {
-            item: {
-              id: "item-1",
-              name: "item-1-name",
-              receivingUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-              shippingUnit: {
-                id: "unit-2",
-                name: "Kg",
-                conversionFactor: 1000,
-              },
-              stockUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-              baseUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-            },
-            quantity: 2,
-          },
-          {
-            item: {
-              id: "item-1",
-              name: "item-1-name",
-              receivingUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-              shippingUnit: {
-                id: "unit-2",
-                name: "Kg",
-                conversionFactor: 1000,
-              },
-              stockUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-              baseUnit: {
-                id: "unit-1",
-                name: "g",
-                conversionFactor: 1,
-              },
-            },
-            quantity: 3,
-          },
-        ],
-      }),
-      itemMsts: [
+  setup(){
+    const actDate = ref(new Date());
+    const recieveFrom = ref("");
+    const recievingDataArray = ref([]);
+
+    const addRecievingData = () => {
+      recievingDataArray.value.push({id: createUUID(),item: itemMsts[0], quantity: 0});
+    };
+
+    const removerecievingData = (index) => {
+      recievingDataArray.value.splice(index, 1);
+    };
+
+    const onSubmit = () => {
+      console.log(this.recievingItemID);
+    };
+
+    const itemMsts= [
         {
           id: "item-1",
           name: "item-1-name",
@@ -163,20 +122,11 @@ export default {
             conversionFactor: 1,
           },
         },
-      ],
-    };
-  },
-  methods: {
-    addRecievingItem() {
-      //const imst = new ItemMST()
-      const item = new Item("", item, 0);
-
-      this.recievingItems.push();
-    },
-    onSubmit() {
-      console.log(this.recievingItemID);
-    },
-  },
+      ];
+      
+    return {actDate, recieveFrom, recievingDataArray, itemMsts, addRecievingData, removerecievingData, onSubmit};
+  }
+ 
 };
 </script>
 
