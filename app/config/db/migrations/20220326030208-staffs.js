67c98166de7a56c6ec21e6471bcf72eafd801bcf
wazatoki <wazatoki@gmail.com>
2022-03-30 11:26:56 +0000

    create dbmigration staffs

---
+++ app/config/db/migrations/20220326030208-staffs.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db) {
+  return db.createTable('staffs', {
+    id: { type: 'string', primaryKey: true },
+    del: {type: 'boolean', defaultValue: false},
+    created_at: 'timestamp',
+    created_by: 'string',
+    operated_at: 'timestamp',
+    operated_by: 'string', 
+    user_id: {type: 'string', notNull: true },
+    password: {type: 'string', notNull: true }
+  });
+};
+
+exports.down = function(db) {
+  return db.dropTable('staffs');
+};
+
+exports._meta = {
+  "version": 1
+};
